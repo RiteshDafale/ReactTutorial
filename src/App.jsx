98e8319635613ed8f82d61b38fde13faf8c300e9
Ritesh Dafale <ritesh.dafale13@gmail.com>
2025-04-16 19:11:47 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import First from './Component/First'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div>
-    {/* <p>homepage</p> */}
-      <First/>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/ReactTutorial' element={<First/>}></Route>
+   </Routes>
+   </BrowserRouter>
+    
     </div>
   )
 }
